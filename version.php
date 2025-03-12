cb97cb6264bd996ba082d9d11fcdf63a451f9fcf
Laurent David <lmedavid@gmail.com>
2025-03-12 20:19:57 +0100

    Update to version 2.5.1

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025011000;      // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2025011001;      // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2020061501;      // Requires this Moodle version (3.9.1).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '2.5.0'; // New API.
+$plugin->release = '2.5.1'; // New API.
 $plugin->component = 'local_competvet';// Full name of the plugin (used for diagnostics).
 $plugin->cron = 0;
 $plugin->dependencies = [
